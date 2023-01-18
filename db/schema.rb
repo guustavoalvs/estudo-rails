0478d5c662512c4db2c5fd7833c1f166323936c5
Gustavo Alves <gustavoalves12ag@gmail.com>
2023-01-18 13:25:18 -0300

    Adicionado pessoa

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_18_161601) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_18_162027) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_18_161601) do
 
   create_table "notes", force: :cascade do |t|
     t.string "name", limit: 120, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "people", force: :cascade do |t|
+    t.string "name", limit: 100, null: false
+    t.string "document", limit: 11, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
